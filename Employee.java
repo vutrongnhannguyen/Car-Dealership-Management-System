bff90ab3990794ff6b45853091f16b08756690ba
vutrongnhannguyen <nvtrongnhann@gmail.com>
2024-07-27 00:23:55 +0700

    [ADD] Employee class

---
+++ Employee.java
@@ -0,0 +1,9 @@
+public class Employee extends User {
+    public Employee(String userId, String fullName, String dateOfBirth, String address, String phoneNumber, String email, String userType, String status, String password) {
+        super(userId, fullName, dateOfBirth, address, phoneNumber, email, userType, status, password);
+    }
+
+    public void viewOwnLogs() {
+
+    }
+}
