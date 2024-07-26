2e53673449eb7f88d91805c27808e83a99db8270
vutrongnhannguyen <nvtrongnhann@gmail.com>
2024-07-27 00:24:05 +0700

    [ADD] Manager class

---
+++ Manager.java
@@ -0,0 +1,15 @@
+public class Manager extends User{
+    public Manager(String userId, String fullName, String dateOfBirth, String address, String phoneNumber,
+                   String email, String userType, String status, String password) {
+        super(userId, fullName, dateOfBirth, address, phoneNumber, email, userType, status, password);
+    }
+
+    // Manager - specific methods
+    public void viewAllEntites(){
+
+    }
+
+    public void generateStatistics(){
+
+    }
+}
