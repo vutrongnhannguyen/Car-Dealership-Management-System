0234444062e843adcbd1b47825a89cb67333b5e8
vutrongnhannguyen <nvtrongnhann@gmail.com>
2024-07-27 00:24:11 +0700

    [ADD] Mechanic class

---
+++ Mechanic.java
@@ -0,0 +1,6 @@
+public abstract class Mechanic extends Employee{
+
+    public Mechanic(String userId, String fullName, String dateOfBirth, String address, String phoneNumber, String email, String userType, String status, String password) {
+        super(userId, fullName, dateOfBirth, address, phoneNumber, email, userType, status, password);
+    }
+}
