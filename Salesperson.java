cbf6327a23cd896f71b91776f9d20b5a8e173657
vutrongnhannguyen <nvtrongnhann@gmail.com>
2024-07-27 00:24:20 +0700

    [ADD] Salesperson class

---
+++ Salesperson.java
@@ -0,0 +1,6 @@
+public abstract class Salesperson extends Employee{
+
+    public Salesperson(String userId, String fullName, String dateOfBirth, String address, String phoneNumber, String email, String userType, String status, String password) {
+        super(userId, fullName, dateOfBirth, address, phoneNumber, email, userType, status, password);
+    }
+}
