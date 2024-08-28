ce7ab91ae02d777c01fb720529ed4ff371db3457
Khoa Duong <khoaduong719@gmail.com>
2024-08-28 08:15:40 +0200

    [UPDATE] Split Code from Main.java

--- Main.java
+++ Main.java
@@ -14,15 +14,7 @@ public class Main {
     private static List<Transaction> transactions = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println("--------------------------");
-        System.out.println("COSC2081 GROUP ASSIGNMENT");
-        System.out.println("AUTO168 CAR DEALERSHIP MANAGEMENT SYSTEM");
-        System.out.println("Instructor: Mr. Minh Vu & Mr. Dung Nguyen");
-        System.out.println("Group: Team 4");
-        System.out.println("s, Chung Chi Vi");
-        System.out.println("s3864111, Duong Hoang Anh Khoa");
-        System.out.println("s, Nguyen Vu Trong Nhan");
-        System.out.println("--------------------------");
+        WelcomeScreen.display();
 
         // Load data from CSV files
         cars = FileManager.readCars("D:/programming1/asm3/cars.csv");
