1da89f3f22be5b8577c8d08e813864cc5df0c804
anhkhoa31 <khoaduong719@gmail.com>
2024-09-11 10:02:46 +0200

    [UPDATE] group name for WelcomeScreen.java

--- Screen/WelcomeScreen.java
+++ Screen/WelcomeScreen.java
@@ -7,7 +7,7 @@ public class WelcomeScreen {
         System.out.println("AUTO168 CAR DEALERSHIP MANAGEMENT SYSTEM");
         System.out.println("**********************************");
         System.out.println("Instructor: Mr. Minh Vu & Mr. Dung Nguyen");
-        System.out.println("Group: Group Name");
+        System.out.println("Group: Group 4");
         System.out.println("s4028019, Nguyen Vu Trong Nhan");
         System.out.println("s3979361, Chung Chi Vi");
         System.out.println("s3864111, Duong Hoang Anh Khoa");
